ca7e4afb2ec81fe80e159525cf2380cf4e5e3177
SZEDO-Scripts <szedoscripts01@gmail.com>
2025-04-21 18:41:52 +0200

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,28 @@
+Config = {}
+
+Config.Blips = {
+    {
+        coords = vector3(215.76, -810.12, 30.73),
+        name = "Ruhabolt",
+        sprite = 73,       -- Blip ikon
+        color = 2,         -- Szín
+        scale = 0.8,       -- Méret
+        shortRange = true  -- Csak közelről látszik
+    },
+    {
+        coords = vector3(-42.4, -1749.2, 29.42),
+        name = "Bolt",
+        sprite = 52,
+        color = 5,
+        scale = 0.8,
+        shortRange = true
+    },
+    {
+        coords = vector3(452.6, -973.2, 30.7),
+        name = "Rendőrség",
+        sprite = 60,
+        color = 29,
+        scale = 0.9,
+        shortRange = false
+    }
+}
